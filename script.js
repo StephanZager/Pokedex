0cd5fffb60dcc02aa7fb014b4ce311545e36f85d
Stephan Zager <139002675+Blitzi3210@users.noreply.github.com>
2024-04-15 16:16:00 +0200

    Pokemon img

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let pokemon;
 let pokemonDetails;
-
+let pokemonId;
+let pokemonName;
+let pokemonImg;
 
 
 
@@ -12,17 +14,31 @@ async function loadPokemon() {
     pokemon = responseAsJson['results'];
    
     console.log(pokemon);
-    loadPokemoninformation();
+    loadPokemonInformation();
 }
 
-async function loadPokemoninformation(){
+async function loadPokemonInformation(){
     for (let i = 0; i < pokemon.length; i++) {
         let url = 'https://pokeapi.co/api/v2/pokemon/'+ (i + 1);
         let response = await fetch(url);
         pokemonDetails = await response.json();
+        pokemonId = pokemonDetails['id'];
+        pokemonName = pokemonDetails['name'];
+        pokemonImg = pokemonDetails['sprites']['other']['dream_world']['front_default'];
+        console.log(pokemonName);        
+        console.log(pokemonImg);
         console.log(pokemonDetails);
-    }
+        test(i, pokemonImg);    
+    }   
+    
 }
+
+function test(index, img){
+    let testElement = document.getElementById('test');
+    testElement.innerHTML += `<div>${index + 1}</div><img src="${img}" alt="Pokemon Image"><br>`;
+}
+
+
 
 
 
